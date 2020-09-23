461ac3b85e25b9094885a949a6a96d2ecb01a7b3
damiangrande <dgrande1@studenti.uninsubria.it>
2020-09-23 17:15:06 +0200

    Esteso test end to end.

--- end_to_end_test.py
+++ end_to_end_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +15,21 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        input_box.send_keys('Start playing Metal Gear Solid.')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Start playing Metal Gear Solid.' for row in rows))
+
+        self.fail('This test is not finished yet.')
 
 
 if __name__ == '__main__':
