5de0a85a85262a1c8021f87bddd67301e37ece8b
damiangrande <dgrande1@studenti.uninsubria.it>
2020-10-01 10:35:38 +0200

    Aggiunto test funzionale il quale si aspetta che ogni utente abbia la sua lista.

--- end_to_end_tests/test.py
+++ end_to_end_tests/test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 import unittest
@@ -6,6 +7,7 @@ import time
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAIT = 10
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,12 +15,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.close()
 
-    def _check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def _wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
@@ -39,8 +49,39 @@ class NewVisitorTest(LiveServerTestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self._check_for_row_in_list_table('1: Start playing Metal Gear Solid.')
-        self._check_for_row_in_list_table('2: Lose weight (for real this time, I swear).')
+        self._wait_for_row_in_list_table('1: Start playing Metal Gear Solid.')
+        self._wait_for_row_in_list_table('2: Lose weight (for real this time, I swear).')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Clean the bathroom.')
+        input_box.send_keys(Keys.ENTER)
+        self._wait_for_row_in_list_table('1: Clean the bathroom.')
+
+        url_user_1 = self.browser.current_url
+        self.assertRegex(url_user_1, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Clean the bathroom.', page_text)
+        self.assertNotIn('Start playing Metal Gear Solid.')
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Looking for a new job without pairing.')
+        input_box.send_keys(Keys.ENTER)
+        self._wait_for_row_in_list_table('1: Looking for a new job without pairing.')
+
+        url_user_2 = self.browser.current_url
+        self.assertRegex(url_user_2, '/lists/.+')
+
+        self.assertNotEqual(url_user_1, url_user_2)
+
+        self.assertNotIn('Clean the bathroom', page_text)
+        self.assertIn('Looking for a new job without pairing.')
 
 
 if __name__ == '__main__':
