b36a75dfb4e86b8a1853ffceeecb6ee8751df0b7
damiangrande <dgrande1@studenti.uninsubria.it>
2020-10-26 12:42:06 +0100

    Aggiunto test funzionale per il layout e lo stile.

--- end_to_end_tests/test.py
+++ end_to_end_tests/test.py
@@ -85,6 +85,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Clean the bathroom', page_text)
         self.assertIn('Looking for a new job without pairing.', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 512, delta=10)
+
+        input_box.send_keys('Test.')
+        input_box.send_keys(Keys.ENTER)
+        self._wait_for_row_in_list_table('1: Test.')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width'] / 2, 512, delta=10)
+
 
 if __name__ == '__main__':
     unittest.main()
