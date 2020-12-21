eda563e2b25719fecff46fa0f57698e180c47124
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2020-12-21 11:51:21 +0100

    Set LOGGING_CONFIG to None.

--- superlists/settings.py
+++ superlists/settings.py
@@ -28,6 +28,8 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+LOGGING_CONFIG = None
+
 INSTALLED_APPS = [
     # 'django.contrib.admin',
     'django.contrib.auth',
@@ -72,17 +74,13 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
 
-    DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
